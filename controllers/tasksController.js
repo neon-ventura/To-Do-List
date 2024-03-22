679ea10782825a51a59d7b877bf2abec8b44872c
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-03-21 22:35:01 -0300

    Fiz a home do To do List, e as funções (faltando a de edição e de Done)
    Finalizarei as funções e o CSS amanhã :)

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,7 +1,34 @@
 const Task = require('../models/Task')
+const { raw } = require('express')
+
 
 module.exports  = class TasksController {
-    static allTasks(req, res) {
-        res.render('tasks')
+
+    static async allTasks(req, res) {
+        const task = await Task.findAll( { raw : true } )
+        console.log(task)
+        res.render('tasks', { task })
+    }
+
+    static async createTask(req, res) {
+        const title = req.body.title
+        const done = '0'
+
+        await Task.create({title, done})
+        res.redirect('/')
+    }
+
+    static async deleteTask(req, res) {
+        const id = req.body.id
+
+        await Task.destroy( { where: { id : id } } )
+        res.redirect('/')
+    }
+
+    static async editTask(req, res) {
+        const id = req.params.id
+        const task = await Task.findOne( { where : { id : id }, raw : true  })
+        console.log(id)
+        res.render('edit-task', { task } )
     }
 }
--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const TasksController = require('../controllers/tasksController')
 
 router.get('/', TasksController.allTasks)
-
+router.get('/edit/:id', TasksController.editTask)
+router.post('/createTask', TasksController.createTask)
+router.post('/deleteTask', TasksController.deleteTask)
 module.exports = router
